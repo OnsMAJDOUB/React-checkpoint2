2dc6e4fd42af2b979dc142f1f5ce9792379d8554
ons majdoub <onesmajdoub99@gmail.com>
2024-09-17 12:28:59 +0300

    first commit

---
+++ src/players.js
@@ -0,0 +1,41 @@
+// src/players.js
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentina",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl:
+      "https://img.a.transfermarkt.technology/portrait/big/28003-1710080339.jpg?lm=1",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al-Nassr",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 38,
+    imageUrl:
+      "https://img.a.transfermarkt.technology/portrait/big/8198-1694609670.jpg?lm=1",
+  },
+  {
+    name: "Neymar Jr.",
+    team: "Al-Hilal",
+    nationality: "Brazil",
+    jerseyNumber: 10,
+    age: 31,
+    imageUrl:
+      "https://b.fssta.com/uploads/application/soccer/headshots/713.png",
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "France",
+    jerseyNumber: 7,
+    age: 25,
+    imageUrl:
+      "https://img.a.transfermarkt.technology/portrait/big/342229-1682683695.jpg?lm=1",
+  },
+];
+
+export default players;
